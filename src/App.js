93c3bee053f19aa73cdac87519d8bbe9393a9526
Ilya <i.veroniys@gmail.com>
2021-02-04 12:33:05 +0300

    add Figure component and basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from './components/Header'
+import Figure from './components/Figure'
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="main-container">
+        <Figure />
+      </div>
+      
     </>
   );
 }
