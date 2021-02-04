03e912446ea95e2a14e797a57781fa1c67d24775
Ilya <i.veroniys@gmail.com>
2021-02-04 12:19:07 +0300

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <>
+         <h1>Gallows game</h1>
+         <h3>Guess the word by entering letters</h3>   
+        </>
+    )
+}
+
+export default Header
